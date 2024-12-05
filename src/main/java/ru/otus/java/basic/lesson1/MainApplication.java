553465441386c032149751a2ada0456bab2a62a8
{Даниил Лежнин} <{bydana94@yandex.ru}>
2024-12-05 12:10:21 +0500

    научились выводить числа в консоль

--- src/main/java/ru/otus/java/basic/lesson1/MainApplication.java
+++ src/main/java/ru/otus/java/basic/lesson1/MainApplication.java
@@ -3,5 +3,8 @@ package ru.otus.java.basic.lesson1;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println ("Hello World");
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
     }
 }
